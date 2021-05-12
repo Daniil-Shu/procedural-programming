0672975d6e58663c0c80c68e81bc2a410d4c0ee4
Daniil-Shu <79913091+Daniil-Shu@users.noreply.github.com>
2021-05-12 15:19:52 +0300

    Revert "No master"

--- 1-2.cpp
+++ 1-2.cpp
@@ -2,46 +2,30 @@
 //  main.cpp
 //  1-2
 //
-//  Created by Даниил Шуриков on 29.04.2021.
+//  Created by Даниил Шуриков on 28.04.2021.
 //
 
 #include <iostream>
 #include <math.h>
+#include <stdio.h>
+
 
 using namespace std;
-class Calculate
-{
-  int r,h ;
 
-public:
-    void getdata()
-    {
-        cout<<"\nВведите радиус и высоту";
-        cin>>r>>h;
-
-    }
-    void Surfacearea()
-    {
-            float s;
-        s = 3.14 * r * (r + h);
-        cout<<"\nПолная поверхность конуса="<<s;
-    }
-   
-    void volume()
-    
-    {
-        float v;
-        v = (3.14 * r * r * h)/3;
-     cout<<"\nОбъёем конуса="<<v;
-    }
-
-};
-Calculate a1,a2,g;
 int main()
+
 {
-g.getdata();
-    a1.getdata();
-    a2.getdata();
-a1.Surfacearea();
-a2.volume();
+
+    int f;
+    int a;
+    int s;
+
+    cin >> a >> f;
+    
+    s=a/f;
+    printf("Определяем путь, пройденный автомобилем. S=a/f\n");
+    printf("f=%d a=%d s=%d\n",f, a, s);
+    
+    return 0;
 }
+
