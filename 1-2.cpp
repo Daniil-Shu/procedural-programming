5f67d82552578f2135ab5f3003ca3d2271268150
DaniilShurikov <daniilshurikov@yandex.ru>
2021-05-17 11:49:43 +0300

    изменил код 1-2

--- 1-2.cpp
+++ 1-2.cpp
@@ -2,30 +2,39 @@
 //  main.cpp
 //  1-2
 //
-//  Created by Даниил Шуриков on 28.04.2021.
+//  Created by Даниил Шуриков on 17.05.2021.
 //
 
 #include <iostream>
-#include <math.h>
-#include <stdio.h>
-
-
+#include <cmath>
+//#include <Windows.h>
 using namespace std;
 
-int main()
+double GetVolume(const double r, const double a);
+double GetSquare(const double r, const double a);
 
-{
-
-    int f;
-    int a;
-    int s;
-
-    cin >> a >> f;
-    
-    s=a/f;
-    printf("Определяем путь, пройденный автомобилем. S=a/f\n");
-    printf("f=%d a=%d s=%d\n",f, a, s);
-    
-    return 0;
+int main() {
+    //SetConsoleOutputCP(1251);
+   // SetConsoleCP(1251);
+    double r;
+    double a;
+    cout << "Введите радиус сферы и угол при вершине конуса (в радианах):\n";
+    cin >> r;
+    cin >> a;
+    auto volume = GetVolume(r, a);
+    auto square = GetSquare(r, a);
+    cout << "Объем конуса равен: " << volume << endl;
+    cout << "Полна площадь поверхности конуса равна: " << square;
 }
 
+double GetVolume(const double r, const double a) {
+    const double pi = 3.141592653589793;
+    auto volume = pi * pow(r, 3) * (1 + cos(a)) * pow(sin(2 * a), 2) / 3;
+    return volume;
+}
+
+double GetSquare(const double r, const double a) {
+    const double pi = 3.141592653589793;
+    auto square = pi * pow(r, 2) * sin(2 * a) * (sin(2 * a) + 2 * cos(a));
+    return square;
+}
