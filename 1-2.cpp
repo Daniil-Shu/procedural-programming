33116ffb7ee5bc14edeb5b9e3d7ac0c1ca0c89a1
MrPrafodi <daniilshurikov@yandex.ru>
2021-05-26 15:53:52 +0300

    дополнил изменения 1-2

--- 1-2.cpp
+++ 1-2.cpp
@@ -4,15 +4,28 @@
 //
 //  Created by Даниил Шуриков on 17.05.2021.
 //
-
+#define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
 using namespace std;
 
+/**
+*\brief Функция для расчета объема конуса
+*\param r Радиус сферы
+*\param a Угол при вершине конуса
+*\return Объем конуса
+**/
 double GetVolume(const double r, const double a);
+/**
+*\brief Функция для расчета полной площади поверхности коунса
+*\param r Радиус сферы
+*\param a Угол при вершине конуса
+*\return Полная площадь поверхности конуса
+**/
 double GetSquare(const double r, const double a);
 
-int main() {
+int main()
+{
     double r;
     double a;
     cout << "Введите радиус сферы и угол при вершине конуса (в радианах):\n";
@@ -24,14 +37,14 @@ int main() {
     cout << "Полна площадь поверхности конуса равна: " << square;
 }
 
-double GetVolume(const double r, const double a) {
-    const double M_PI;
+double GetVolume(const double r, const double a)
+{
     auto volume = M_PI * pow(r, 3) * (1 + cos(a)) * pow(sin(2 * a), 2) / 3;
     return volume;
 }
 
-double GetSquare(const double r, const double a) {
-    const double M_PI;
+double GetSquare(const double r, const double a)
+{
     auto square = M_PI * pow(r, 2) * sin(2 * a) * (sin(2 * a) + 2 * cos(a));
     return square;
 }
